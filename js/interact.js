039587ab41e8b1d5261a8e83312694d6d68df21a
Miscthings <miscthings1@gmail.com>
2016-04-07 14:53:40 -0400

    Added files via upload
    Header & Nav revision

--- js/interact.js
+++ js/interact.js
@@ -1,12 +1,22 @@
-$(document).ready(function() {
-  $("#slideControls").on('click', 'span', function() {
-    $("#slideApp div").removeClass("opaque");
 
-    var newImage = $(this).index();
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
 
-    $("#slideApp div").eq(newImage).addClass("opaque");
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
 
-    $("#slideControls span").removeClass("selected");
-    $(this).addClass("selected");
-  });
-});
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
